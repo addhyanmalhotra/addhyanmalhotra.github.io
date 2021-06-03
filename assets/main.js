55327cd5611c3072e8aa7612a6eb37dd40d2ace1
addhyanmalhotra <addhyanmalhotra@gmail.com>
2021-06-03 16:25:30 +0530

    big overhaul

--- assets/main.js
+++ assets/main.js
@@ -1,61 +1,54 @@
-var paperMenu = {
-	$window: $('#paper-window'),
-	$paperFront: $('#paper-front'),
-	$hamburger: $('.hamburger'),
-	offset: 1800,
-	pageHeight: $('#paper-front').outerHeight(),
+document.addEventListener('DOMContentLoaded',function(event){
+	var sound_check_box = document.getElementById("sound");
+	sound_check_box.checked = true;
+	var audio = new Audio('keyboard.mp3');
+	sound_check_box.onchange = function() {
+		if(!this.checked) {
+		}
+		else{
+			audio.pause();
+		}
+   };
+	// array with texts to type in typewriter
+	var dataText = [ "Web Developement.","Comp Sci.", "Engineering.","Graphic Design.", "Mathematics.", "Addhyan Malhotra!"];
 	
-	open: function() {
-        $('.alert').alert('close');
-		this.$window.addClass('tilt');
-		this.$hamburger.off('click');
-
-		$('#container, .hamburger, #ham').on('click', this.close.bind(this));
-		this.hamburgerFix(true);
-		console.log('opening...');
-	},
-	close: function() {
-		this.$window.removeClass('tilt'); 
-		$('#container, .hamburger').off('click');
-		this.$hamburger.on('click', this.open.bind(this));
-		this.hamburgerFix(false);
-		console.log('closing...');
-	},
-	updateTransformOrigin: function() {
-		scrollTop = this.$window.scrollTop();
-		equation = (scrollTop + this.offset) / this.pageHeight * 100;
-		this.$paperFront.css('transform-origin', 'center ' + equation + '%');
-	},
-	//hamburger icon fix to keep its position
-	hamburgerFix: function(opening) {
-			if(opening) {
-				$('.hamburger').css({
-					position: 'absolute',
-					top: this.$window.scrollTop() + 30 + 'px'
-				});
-			} else {
-				setTimeout(function() {
-					$('.hamburger').css({
-						position: 'fixed',
-						top: '30px'
-					});
-				}, 300);
-			}
-		},
-	bindEvents: function() {
-		this.$hamburger.on('click', this.open.bind(this));
-		$('.close_nav').on('click', this.close.bind(this));
-		this.$window.on('scroll', this.updateTransformOrigin.bind(this));
-	},
-	init: function() {
-		this.bindEvents();
-		this.updateTransformOrigin();
-	},
-};
-
-paperMenu.init();
-function clickHam() {
-    $('#alerts').append(
-        '<div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>DO NOT CLICK ME!!<br></strong> Try clicking the nav button instead <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
-    );
-}
+	// type one text in the typwriter
+	// keeps calling itself until the text is finished
+	function typeWriter(text, i, fnCallback) {
+	  // chekc if text isn't finished yet
+	  if (i < (text.length)) {
+		// add next character to h1
+	  	if(!sound_check_box.checked)audio.play();
+	   document.querySelector("#h1").innerHTML = text.substring(0, i+1) +'<span class="type-writer-span" aria-hidden="true"></span>';
+  
+		// wait for a while and call this function again for next character
+		setTimeout(function() {
+		  typeWriter(text, i + 1, fnCallback)
+		}, 100);
+	  }
+	  // text finished, call callback if there is a callback function
+	  else if (typeof fnCallback == 'function') {
+		// call callback after timeout
+		audio.pause();
+		setTimeout(fnCallback, 700);
+	  }
+	}
+	// start a typewriter animation for a text in the dataText array
+	 function StartTextAnimation(i) {
+	   if (typeof dataText[i] == 'undefined'){
+		  setTimeout(function() {
+			StartTextAnimation(0);
+		  }, 20000);
+	   }
+	   // check if dataText[i] exists
+	  if (i < dataText[i].length) {
+		// text exists! start typewriter animation
+	   typeWriter(dataText[i], 0, function(){
+		 // after callback (and whole text has been animated), start next text
+		 StartTextAnimation(i + 1);
+	   });
+	  }
+	}
+	// start the text animation
+	StartTextAnimation(0);
+});
